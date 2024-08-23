883ce1036759f082c73e51d0a9c818ed0f5e74ff
Mochamad Akbar <mochakbar912@gmail.com>
2024-08-23 15:44:46 +0700

    initial commit

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+    // boolean, uint, int, address, bytes
+    // bool hasFavoriteNumber = false;
+    uint256 public favoriteNumber;
+    // People public person = People({favoriteNumber: 2, name: "Jhon"});
+    mapping(string => uint256) public nameToFavoriteNumber;
+    // string favoriteNumberInText = "Five";
+    // int256 favoriteInt = -5;
+    // bytes favoriteBytes = "cat";
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrive() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
