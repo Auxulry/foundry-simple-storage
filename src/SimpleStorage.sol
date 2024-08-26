766bd70571e2ad84ad2fad7a45abb9c3bfc352f3
Mochamad Akbar <mochakbar912@gmail.com>
2024-08-26 14:34:47 +0700

    initial commit

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 contract SimpleStorage {
     // boolean, uint, int, address, bytes
@@ -22,7 +22,7 @@ contract SimpleStorage {
         favoriteNumber = _favoriteNumber;
     }
 
-    function retrive() public view returns (uint256) {
+    function retrieve() public view returns (uint256) {
         return favoriteNumber;
     }
 
